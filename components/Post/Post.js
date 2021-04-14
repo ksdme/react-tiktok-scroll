994ae78db54348127fd4385969c691433569e1e5
Kilari Teja <kilariteja9@gmail.com>
2021-04-15 01:16:26 +0530

    Post: Update design and use name from prop

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,16 +1,18 @@
 export default function Post({ post }) {
   return (
-    <div className="p-8">
-      <h2 className="font-bold text-xl text-black">
-        Post #0
-      </h2>
+    <div className="w-screen h-screen flex items-center">
+      <div className="p-8">
+        <h2 className="font-bold text-xl text-black">
+          {post.name}
+        </h2>
 
-      <div className="mt-2 text-black max-w-sm">
-        By default it adds a set of tap, doubletap, press,
-        horizontal pan and swipe, and the multi-touch pinch
-        and rotate recognizers. The pinch and rotate recognizers
-        are disabled by default because they would make the
-        element blocking, but you can enable them by calling:
+        <div className="mt-2 text-black max-w-sm">
+          By default it adds a set of tap, doubletap, press,
+          horizontal pan and swipe, and the multi-touch pinch
+          and rotate recognizers. The pinch and rotate recognizers
+          are disabled by default because they would make the
+          element blocking, but you can enable them by calling:
+        </div>
       </div>
     </div>
   )

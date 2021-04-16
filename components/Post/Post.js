0bbb447daaf73e664d0bab6f3b750b05c44a5a41
Kilari Teja <kilariteja9@gmail.com>
2021-04-17 03:17:07 +0530

    Increase default placeholder text size

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -12,6 +12,16 @@ export default function Post({ post }) {
           and rotate recognizers. The pinch and rotate recognizers
           are disabled by default because they would make the
           element blocking, but you can enable them by calling:
+          By default it adds a set of tap, doubletap, press,
+          horizontal pan and swipe, and the multi-touch pinch
+          and rotate recognizers. The pinch and rotate recognizers
+          are disabled by default because they would make the
+          element blocking, but you can enable them by calling:
+          By default it adds a set of tap, doubletap, press,
+          horizontal pan and swipe, and the multi-touch pinch
+          and rotate recognizers. The pinch and rotate recognizers
+          are disabled by default because they would make the
+          element blocking, but you can enable them by calling:
         </div>
       </div>
     </div>
